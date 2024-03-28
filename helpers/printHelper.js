3706515248d773edcee10de9d3e8b29513235803
mnkollo <73963114+mnkollo@users.noreply.github.com>
2024-03-27 19:16:34 -0500

    Update printHelper.js

--- helpers/printHelper.js
+++ helpers/printHelper.js
@@ -4,15 +4,25 @@ export function printAge(age){
 }
 
  class CustomerDetails{  
+
     printFirstName(firstName){
         console.log("First Name: " + firstName);
     }
 
-
-    printLastName(firstName){
-        console.log("First Name: " + firstName);
+    /**
+     * this method will print the last name
+     * @param {*} lastName 
+     */
+    printLastName(lastName){
+        console.log("Last Name: " + lastName);
     }
 }
 
 
 export var customerDetails = new CustomerDetails();   // export the class
+
+//created an instance of the class and exported it.
+
+
+//In JavaScript, the term "instance" typically refers to an individual occurrence or 
+//realization of an object that is created using a constructor function or a class.
